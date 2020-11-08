30186b8fafe9fe400561d8e9cb3ba513146bf5e2
Shreyansh1808 <73379925+Shreyansh1808@users.noreply.github.com>
2020-11-08 11:35:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var fixedRect, object1;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  
+}
+
+function draw() {
+  background(0,0,0);  
+//bounceOff(movingRect,fixedRect);
+if(isTouching(movingRect, fixedRect))
+{
+  movingRect.shapeColor = "blue";
+  fixedRect.shapeColor = "blue";
+}
+else
+{
+  movingRect.shapeColor = "green";
+ fixedRect.shapeColor = "green";
+}
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
